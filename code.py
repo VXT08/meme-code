ed5bdc7eaa9b31f59a2ec39c48f231443f91cf5d
VXT08 <117527688+VXT08@users.noreply.github.com>
2023-05-21 10:54:39 +0300

    Update and rename README.md to code.py

---
+++ code.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def mem(ctx):
+    with open('images/mem1.jpg', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+bot.run("token")
