7a12b3818528b4d4714e038d6ff093785002299a
VXT08 <117527688+VXT08@users.noreply.github.com>
2023-05-25 22:18:17 +0300

    Add files via upload

---
+++ Homework/botekAN.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import random
+import os
+print(os.listdir('animal'))
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def animmem(ctx):
+    
+    images = os.listdir('animal') 
+    img_name = random.choice(images)
+    
+    with open(f'animal/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+
+bot.run("MTEwNDY3MzcxOTI2MTA3MzUwOA.G97sVE.77S6vKgLQl53plIC6ktxdju8ns9jjHYD7OYp58")
